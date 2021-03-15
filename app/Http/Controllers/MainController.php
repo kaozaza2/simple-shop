a7a87b3a2e3a33e2a21145e0cddb51796a3a4bc9
Moki <kittisakphiri@gmail.com>
2021-03-15 19:06:14 +0700

    cartUpdate

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Cart;
 use App\Models\Product;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -28,47 +27,71 @@ class MainController extends Controller
 
     public function addcart(Request $request)
     {
-        $amount = $request->input('amount');
-        $product = Product::find($request->input('product'));
-        $cart = collect($request->session()->pull('cart'));
-        if ($cart->contains('product.id', $product->id)) {
-            $cart->transform(function ($item) use ($product, $amount) {
-                if ($item->product->id == $product->id) {
-                    $item->amount += $amount;
-                }
-                return $item;
-            });
+        $amount = $request->amount;
+        $product = Product::find($request->product);
+        $cart = collect($request->session()->get('cart', []));
+        if ($item = $cart->firstWhere('product.id', $product->id)) {
+            $item->amount += $amount;
         } else {
-            $cart->push(Cart::addNew($product, $amount));
+            $cart->push(Cart::createItem($product, $amount));
         }
         $request->session()->put('cart', $cart);
         $request->session()->put('message', "{$product->name} $amount ชิ้น ถูกเพิ่มลงตะกร้า");
         return redirect()->route('index');
     }
 
-    public function updateCart(Request $request): JsonResponse
+    public function updateCart(Request $request)
     {
-        $cart = collect($request->session()->get('cart'));
-        if ($cart->contains('product.id', $request->product)) {
-            foreach ($cart as $key => $val) {
-                if ($val->product->id == $request->product) {
-                    $val->amount = $request->amount;
-                    $cart->put($key, $val);
-                }
+        $amount = $request->amount;
+        $product = Product::find($request->product);
+        $cart = collect($request->session()->get('cart', []));
+        if ($item = $cart->firstWhere('product.id', $product->id)) {
+            if ($request->mode == 'force') {
+                $item->amount = intval($amount);
+            } else {
+                $item->amount += $amount;
             }
-            return response()->json([$val, $cart->sum(function ($item) {
-                return $item->product->price * $item->amount;
-            })]);
+            if ($item->amount <= 0) {
+                $key = $cart->search(function ($i) use ($item) {
+                    return $i->product->id == $item->product->id;
+                });
+                $cart->forget($key);
+                $item = null;
+            }
+            $request->session()->put('cart', $cart);
+            if (count($cart) == 0) {
+                return response()->json(null, 201);
+            } else {
+                $total = $cart->sum(function ($i) {
+                    return $i->product->price * $i->amount;
+                });
+                return response()->json(['data' => $item, 'total' => $total]);
+            }
         }
-        return response()->json([], 400);
+        return response()->json(null, 404);
     }
 
     public function cart(Request $request)
     {
-        $items = collect($request->session()->get('cart'));
-        $total = $items->sum(function ($item) {
-            return $item->product->price * $item->amount;
+        $items = collect($request->session()->get('cart', []));
+        $total = $items->sum(function ($i) {
+            return $i->product->price * $i->amount;
         });
         return view('cart')->with('items', $items)->with('total', $total);
+    }
+
+    private function array_deep_search($value, $key, $array) {
+        $skey = explode('.', $key);
+        $count = empty($key) ? 0 : count($skey);
+        foreach ($array as $inkey => $invalue) {
+            $subvalue = $invalue;
+            for ($i = 0; $i < $count; $i++) {
+                $subvalue = $subvalue[$skey[$i]];
+            }
+            if ($subvalue == $value) {
+                return $inkey;
+            }
+        }
+        return false;
     }
 }
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -2,61 +2,81 @@
 
 @section('content')
 <div class="container py-2">
-    <table class="table table-striped">
-        <tr class="table-dark text-center">
-            <th>รูป</th>
-            <th>ชื่อสินค้า</th>
-            <th>จำนวน</th>
-            <th>ราคา/ชิ้น</th>
-            <th>อื่นๆ</th>
-        </tr>
-        @foreach ($items as $item)
-        <tr>
-            <td class="text-center">
-                <img class="img-fluid img-thumbnail" style="width: 50px;"
-                     src="/storage/images/{{ $item->product->image }}">
-            </td>
-            <td>{{ $item->product->name }}</td>
-            <td class="text-end" id="amount-{{ $loop->index }}">{{ $item->amount }}</td>
-            <td class="text-end">฿ {{ $item->product->price }}</td>
-            <td class="text-end">
-                <button class="btn btn-success" onclick="updateCart('{{ $item->product->id }}', 1, 'amount-{{ $loop->index }}')">SetTo1</button>
-            </td>
-        </tr>
-        @endforeach
-        @if (count($items) > 0)
-        <tr>
-            <td class="text-end fw-bold" colspan="3">รวม</td>
-            <td class="text-end" id="total">฿ {{ $total }}</td>
-            <td></td>
-        </tr>
-        @else
-        <tr>
-            <td class="text-center" colspan="4">ยังไม่มีสินค้าในตะกร้า</td>
-        </tr>
-        @endif
-    </table>
-    <a href="{{ route('checkout') }}" class="btn btn-success float-end">ชำระเงิน</a>
+  <table id="cart-product" class="table table-striped">
+    <tr class="table-dark text-center">
+      <th>รูป</th>
+      <th>ชื่อสินค้า</th>
+      <th scope="col">จำนวน</th>
+      <th scope="col">ราคา/ชิ้น</th>
+    </tr>
+    @foreach ($items as $item)
+    <tr id="product-{{ $item->product->id }}">
+      <td class="text-center">
+        <img class="img-fluid img-thumbnail" style="width: 50px;"
+           src="/storage/images/{{ $item->product->image }}">
+      </td>
+      <td>{{ $item->product->name }}</td>
+      <td class="text-end">
+        <div class="btn-group btn-group-sm">
+          <button class="btn btn-success" onclick="updateCart('{{ $item->product->id }}', 1)"><i class="fas fa-plus"></i></button>
+          <button id="amount-{{ $item->product->id }}" class="btn btn-outline-dark text-dark" disabled>{{ $item->amount }}</button>
+          <button class="btn btn-success" onclick="updateCart('{{ $item->product->id }}', -1)"><i class="fas fa-minus"></i></button>
+          <button class="btn btn-danger" onclick="warnUpdate('{{ $item->product->id }}', 0)"><i class="fas fa-trash-alt"></i></button>
+        </div>
+      </td>
+      <td class="text-end">฿ {{ $item->product->price }}</td>
+    </tr>
+    @endforeach
+    @if ($items->isNotEmpty())
+    <tr id="product-total">
+      <td class="text-center fw-bold" colspan="2">รวม</td>
+      <td class="text-end" colspan="2" id="total">฿ {{ $total }}</td>
+    </tr>
+    @endif
+    <tr id="product-empty" class="@if ($items->isNotEmpty()) d-none @endif">
+      <td class="text-center" colspan="4">ยังไม่มีสินค้าในตะกร้า</td>
+    </tr>
+  </table>
+  <a id="product-checkout" href="{{ route('checkout') }}" class="btn btn-success float-end @if ($items->isEmpty()) d-none @endif">ชำระเงิน</a>
 </div>
 <script>
-    function updateCart(product, amount, id) {
-        let request = new XMLHttpRequest();
-        request.onreadystatechange = function () {
-            if (request.readyState === 4 && request.status === 200) {
-                let out = document.getElementById(id);
-                let data = JSON.parse(request.responseText);
-                out.innerText = data[0].amount;
-
-                let total = document.getElementById('total');
-                total.innerText = `฿ ${data[1]}`;
-            }
-        }
-        request.open('POST', '{{ route("updateCart") }}', true);
-        let data = new FormData();
-        data.append('product', product);
-        data.append('amount', amount);
-
-        request.send(data);
+  function warnUpdate(id, amount) {
+    if (confirm('ต้องการลบหรือไม่?')) {
+      updateCart(id, amount, 'force');
     }
+  }
+  function updateCart(id, amount, mode = '') {
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+      if (request.readyState === 4) {
+        let product = document.getElementById(`product-${id}`);
+        if (request.status === 200) {
+          let data = JSON.parse(request.responseText);
+          if (!data.data) {
+            product.classList.add('d-none');
+          } else {
+            let amount = document.getElementById(`amount-${id}`);
+            amount.innerText = data.data.amount;
+          }
+
+          let total = document.getElementById('total');
+          total.innerText = `฿ ${data.total}`;
+        }
+        if (request.status === 201) {
+          product.classList.add('d-none');
+          document.getElementById('product-checkout').classList.add('d-none');
+          document.getElementById('product-total').classList.add('d-none');
+          document.getElementById('product-empty').classList.remove('d-none');
+        }
+      }
+    }
+    request.open('POST', '{{ route("updateCart") }}', true);
+    let data = new FormData();
+    data.append('product', id);
+    data.append('amount', amount);
+    data.append('mode', mode);
+
+    request.send(data);
+  }
 </script>
 @endsection
